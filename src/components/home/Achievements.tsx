9904019a4c9920b69b361175fb3c7e01931b25f0
Om Londhe <oplondhe@gmail.com>
2023-06-12 18:08:22 +0530

    Enhanced the UI

--- src/components/home/Achievements.tsx
+++ src/components/home/Achievements.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 
 function Achievements() {
   return (
-    <div className="grid place-items-center mt-11">
+    <div
+      className="grid place-items-center py-20"
+      style={{
+        background: "linear-gradient(rgb(248, 250, 252), white, white)",
+      }}
+    >
       <div
         className="relative grid place-items-center w-[350px] h-[350px] overflow-visible backdrop-blur-lg bg-[#ffffff55] border-2 border-white p-5 m-2 object-cover flex-shrink-0 shadow rounded-[100%] bg-[url('/blob-inner-image.jpeg')]"
         style={{
