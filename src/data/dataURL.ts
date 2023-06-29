8bf2378a8011b9408ff42193ee33db8157d0566c
Om Londhe <oplondhe@gmail.com>
2023-06-29 21:34:38 +0530

    Made a production build

--- src/data/dataURL.ts
+++ src/data/dataURL.ts
@@ -1,1 +1,4 @@
-export const URL = "http://localhost:3000";
+export const URL =
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "http://localhost:3000"
+    : "/";
