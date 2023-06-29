af8eb7e4330c203a7c95986458fce1ee17aa9b3c
Om Londhe <oplondhe@gmail.com>
2023-06-29 22:37:45 +0530

    After making backend live

--- src/data/dataURL.ts
+++ src/data/dataURL.ts
@@ -1,5 +1,4 @@
 export const URL =
-  //   process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
-  //     ? "http://localhost:3000"
-  //     : "";
-  "";
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "http://localhost:3000"
+    : "https://full-stack-portfolio-backend.vercel.app/";
